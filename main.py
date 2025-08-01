f76f4fcf018771d5ffba070a2df70754b72c7509
dylnhoang <dylann.hoang@gmail.com>
2025-07-31 17:20:46 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
-from backend.routers import match_grants
+from backend.routers import profile, match_grants, auth
 
 app = FastAPI()
+
+app.include_router(profile.router)
 app.include_router(match_grants.router)
+app.include_router(auth.router)
